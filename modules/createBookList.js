fb937e71eeaeaa8c00aff32d22721ad52a8ed851
Yonashenok <yonashenok24@gmail.com>
2023-06-12 21:05:01 +0300

    feat: add book list render feature

---
+++ modules/createBookList.js
@@ -0,0 +1,33 @@
+import saveToLocalStorage from './setLocalStorage.js';
+
+export const booksContainer = document.getElementById('booksContainer');
+const createBookList = (books) => {
+  booksContainer.innerHTML = '';
+  const markUp = books
+    .map(
+      (book) => `
+    <li class="list_item">
+        <p>"${book.title}" by ${book.author}</p>
+        <button class="btn-remove" data-remove="${book.id}" >remove</button>
+    </li>
+    `
+    )
+    .join('');
+
+  booksContainer.insertAdjacentHTML('afterbegin', markUp);
+  const bookElement = document.querySelectorAll('.list_item');
+  const removeButton = document.querySelectorAll('.btn-remove');
+  bookElement.forEach((list) => list.classList.add('book-element'));
+  removeButton.forEach((btn) => btn.classList.add('remove-btn'));
+  removeButton.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const id = e.target.dataset.remove;
+      const newBookList = books.filter((item) => item.id !== id);
+      booksContainer.innerHTML = '';
+      createBookList(newBookList);
+      saveToLocalStorage(newBookList);
+    });
+  });
+};
+
+export default createBookList;
