b43aa0c8af579c3f0463031f2e92af7595f841b4
Yonashenok <yonashenok24@gmail.com>
2023-06-13 17:59:39 +0300

    fix: luxon library

--- modules/timeDate.js
+++ modules/timeDate.js
@@ -3,9 +3,13 @@ import { DateTime } from '../node_modules/luxon/src/luxon.js';
 const time = document.querySelector('.time-date');
 
 const setTimeDate = () => {
-  const dt = DateTime.now();
-  const localTime = dt.toLocaleString(DateTime.DATETIME_MED);
-  time.textContent = localTime;
+  // const dt = DateTime.now();
+  // const localTime = dt.toLocaleString(DateTime.DATETIME_MED);
+  // time.textContent = localTime;
+  const dateTime = DateTime.local();
+  const today = dateTime.toLocaleString(DateTime.DATETIME_FULL);
+
+  time.innerHTML = today;
 };
 
 export default setTimeDate;
